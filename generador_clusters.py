b748f5f66df1761716a797b3262d897bb27eb889
RamiroPadilla <ramiro_mdq@live.com>
2025-06-26 22:27:06 -0300

    nuevas instancias cluster

--- generador_clusters.py
+++ generador_clusters.py
@@ -5,31 +5,70 @@ from scipy.spatial.distance import cdist
 import math
 
 #%%
-np.random.seed(234)
 
-n = int(input("Cantidad de puntos:"))
+for rep in range(10,11):
 
+    np.random.seed(rep)
 
-mod = n // 3
-resto = n % 3
-puntos_por_cluster = [mod + (1 if i < resto else 0) for i in range(3)]  # Suma el resto a los primeros clusters
-centros = np.array([[0, 0], [20, 0], [10, 20]])  
+    n = 15
 
 
-puntos = []
-labels = []
-for i, (centro, n) in enumerate(zip(centros, puntos_por_cluster)):
-    cluster = centro + np.random.randint(-2, 3, size=(n, 2))  # Rango reducido para formar clúster
-    puntos.append(cluster)
-    labels += [i] * n
-
-puntos = np.vstack(puntos)
-labels = np.array(labels)
+    mod = n // 3
+    resto = n % 3
+    puntos_por_cluster = [mod + (1 if i < resto else 0) for i in range(3)]  # Suma el resto a los primeros clusters
+    centros = np.array([[0, 0], [20, 0], [10, 20]])  
 
 
-distancias = cdist(puntos, puntos)
-distancias = np.rint(distancias).astype(int)  # Redondeo a enteros
+    puntos = []
+    labels = []
+    for i, (centro, n) in enumerate(zip(centros, puntos_por_cluster)):
+        cluster = centro + np.random.randint(-2, 3, size=(n, 2))  # Rango reducido para formar clúster
+        puntos.append(cluster)
+        labels += [i] * n
 
+    puntos = np.vstack(puntos)
+    labels = np.array(labels)
+
+
+    distancias = cdist(puntos, puntos)
+    distancias = np.rint(distancias).astype(int)  # Redondeo a enteros
+
+    costo_repartidor = 2
+    d_max = 10
+
+    n = len(puntos)
+    clientes = list(range(2, int(n) + 1))
+
+    with open(f"cluster{rep}b.txt", "w") as f:
+        f.write(str(n) + "\n")  # Cantidad de puntos
+        f.write(str(costo_repartidor) + "\n")
+        f.write(str(d_max) + "\n")
+
+        
+        # 10% de nodos refrigerados 
+        num_refrigerados = max(1, math.ceil(0.10 * len(clientes))) if len(clientes) > 0 else 0
+        refrigerados = set(np.random.choice(clientes, size=num_refrigerados, replace=False))
+
+        f.write(str(len(refrigerados)) + "\n")
+        for nodo in sorted(refrigerados):
+            f.write(str(nodo) + "\n")
+        
+
+
+        # 15% de nodos exclusivos
+        num_exclusivos = max(1, math.ceil(0.15 * len(clientes))) if len(clientes) > 0 else 0
+        exclusivos = set(np.random.choice(clientes, size=num_exclusivos, replace=False))
+        
+        f.write(str(len(exclusivos)) + "\n")
+        for nodo in sorted(exclusivos):
+            f.write(str(nodo) + "\n")
+
+        # Escribir matriz de distancias (i j dij dij)
+        for i in range(n):
+            for j in range(i, n):
+                d = distancias[i, j]
+                linea = f"{i+1} {j+1} {d} {d}\n"
+                f.write(linea)
 
 #%%
 # Graficar scatterplot
@@ -49,48 +88,3 @@ plt.grid(True)
 plt.legend()
 plt.axis('equal')
 plt.show()
-
-#%%
-# Escribir archivo de salida
-costo_repartidor = input("Costo por repartidor: ")
-d_max = input("Distancia máxima (d_max): ")
-
-n = len(puntos)
-clientes = list(range(2, int(n) + 1))
-
-with open("archivo.txt", "w") as f:
-    f.write(str(n) + "\n")  # Cantidad de puntos
-    f.write(str(costo_repartidor) + "\n")
-    f.write(str(d_max) + "\n")
-
-    
-    # 10% de nodos refrigerados 
-    num_refrigerados = max(1, math.ceil(0.10 * len(clientes))) if len(clientes) > 0 else 0
-    refrigerados = set(np.random.choice(clientes, size=num_refrigerados, replace=False))
-
-    f.write(str(len(refrigerados)) + "\n")
-    for nodo in sorted(refrigerados):
-        f.write(str(nodo) + "\n")
-    
-
-
-    # 15% de nodos exclusivos
-    num_exclusivos = max(1, math.ceil(0.15 * len(clientes))) if len(clientes) > 0 else 0
-    exclusivos = set(np.random.choice(clientes, size=num_exclusivos, replace=False))
-    
-    f.write(str(len(exclusivos)) + "\n")
-    for nodo in sorted(exclusivos):
-        f.write(str(nodo) + "\n")
-
-
-
-    # Escribir matriz de distancias (i j dij dij)
-    for i in range(n):
-        for j in range(i, n):
-            d = distancias[i, j]
-            linea = f"{i+1} {j+1} {d} {d}\n"
-            f.write(linea)
-
-print(f"Archivo de salida generado correctamente con {n} puntos: archivo.txt")
-
-# %%
